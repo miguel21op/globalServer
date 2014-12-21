11e3bb2017541662330e69078ac3a71cde6f66fe
Miguel Lemos <miguel21op@gmail.com>
2014-12-21 21:42:28 +0000

    correção menor

--- pushService.php
+++ pushService.php
@@ -3,5 +3,5 @@
 // Nada para já
 // A parte de envio das notificações
 // Liga ao serviço da Apple e Google
-// É preciso ver a quesrtão das credenciais
+// É preciso ver a questão das credenciais
 ?>
