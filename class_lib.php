2a5fbed3586381dbd139e435da07a09a2b7259b2
Miguel Lemos <miguel21op@gmail.com>
2014-12-25 20:15:28 +0000

    Mais umas alterações

--- class_lib.php
+++ class_lib.php
@@ -1,8 +1,16 @@
 <?php
-// criar o objeto
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+error_reporting(0);
+
+// criar a classe
  class person{
-    	  var $name; /* property */
-     
+    	 var $name; /* property */
+        
+         public function __construct($persons_name){
+         	$this->name = $persons_name;
+         }
+         
      // acrescentar métodos (funções)
      	 public function set_name($new_name){
      	 $this->name=$new_name;
@@ -16,10 +24,15 @@
      	
  }
  // o new cria o objecto a partir da classe
- $stefan= new person();
- // $stefan = handle variável; 
- $stefan->set_name("João Pedro");
- echo "O nome do Stefan é:  ". $stefan->get_name();
+// $stefan= new person();
+ $jimmy = new person;
+ //$stefan = handle variável; 
+// $stefan->set_name("João Pedro");
+ // ou for short
+ $stefan=new person("João Pedro Matos");
+ $jimmy ->set_name("Jimmy Silva");
+ echo "O nome do Stefan é:  ". $stefan->get_name().'</br>';
+ echo "O nome do Jimmy é:  ". $jimmy->name;
  
 /*
 class MyClass
